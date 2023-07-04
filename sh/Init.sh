2157ffeba6a956089e35407e08fcdc41fec5d4ec
Darian1996 <1934849492@qq.com>
2023-07-04 11:45:07 +0800

    first

---
+++ sh/Init.sh
@@ -0,0 +1,7 @@
+
+
+git init
+git remote add gitee git@gitee.com:Darian1996/notGeekCompany.git
+git remote add github git@github.com:Darian1996/notGeekCompany.git
+git pull gitee master
+git pull github master
